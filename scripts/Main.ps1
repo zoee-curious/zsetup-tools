3f15955ec07889b2a39ef7640e21c58456766dd9
zoee-curious <zoeesjournal@gmail.com>
2026-09-15 19:32:55 +0700

    creating one stand files main.ps1

--- scripts/Main.ps1
+++ scripts/Main.ps1
@@ -1,67 +1,514 @@
 param (
     [string]$Action = "{{ACTION}}",
     [string]$Name = "{{NAME}}",
+    [switch]$Public,
     [switch]$Private,
+    [switch]$Cache,
     [switch]$NoCache,
     [switch]$Help
 )
 
 $BaseUrl = "https://zoee.fun"
 
-function Get-BootstrapHeaders {
-    $Headers = @{}
-    $ConfigFile = Join-Path $HOME ".zsetup"
-    if (Test-Path $ConfigFile) {
-        try {
-            $Config = Get-Content $ConfigFile -Raw | ConvertFrom-Json
-            if ($Config.apiKey) { $Headers["x-api-key"] = $Config.apiKey }
-
-            if ($Private) { $Headers["x-scope"] = 'private' }
-            elseif ($Config.scope) { $Headers["x-scope"] = $Config.scope }
-
-            if ($NoCache) {
-                $Headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-                $Headers["Pragma"] = "no-cache"
-            }
-            elseif ($Config.noCache) {
-                $Headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-                $Headers["Pragma"] = "no-cache"
-            } 
-        }
-        catch {}
-    }
-    return $Headers
-}
+$BaseDir = Join-Path $HOME "zsetup"
+$ShimsDir = Join-Path $BaseDir "shims"
+$ScriptsDir = Join-Path $BaseDir "scripts"
+$LocalDir = Join-Path $BaseDir "apps"
+$TempDir = Join-Path $BaseDir "temp"
 
 $ProgressPreference = 'SilentlyContinue'
-Invoke-Expression (Invoke-RestMethod -Uri "$BaseUrl/getScript/Remotes/Modules.ps1" -Headers (Get-BootstrapHeaders))
-$ProgressPreference = 'Continue'
+$ErrorActionPreference = 'Stop'
 
-if ($PSScriptRoot) {
-    . Get-Modules -BaseUrl $BaseUrl -RootPath $PSScriptRoot -Headers (Get-BootstrapHeaders)
+function Test-Command {
+    param (
+        [string]$Action,
+        [string]$Name,
+        [switch]$Help
+    )
+
+    $invalidAction = [string]::IsNullOrWhiteSpace($Action) -or ($Action -eq "{{ACTION}}")
+    $invalidName = [string]::IsNullOrWhiteSpace($Name) -or ($Name -eq "{{NAME}}")
+
+    if ($invalidAction -or $invalidName) { Show-Help exit }
+    if ($Help) { Show-Help exit }
 }
-else {
-    . Get-Modules -BaseUrl $BaseUrl -Headers (Get-BootstrapHeaders)
+
+function Show-Help {
+    Write-Host
+    Write-FormattedRow -Text "Usage:" -Color Yellow
+    Write-FormattedRow -Text "zst", "<action>", "<name>" -Color Gray, DarkGray, DarkGray -Width 4, 9 -Indent 2
+
+    Write-Host
+    Write-FormattedRow -Text "Action:" -Color Yellow
+    Write-FormattedRow -Text "install", "<name>", ": Install a package (e.g., zst install aria2)" -Color Gray, DarkGray, Gray -Width 8, 14 -Indent 2
+    Write-FormattedRow -Text "search", "<name>", ": Search available manifests" -Color Gray, DarkGray, Gray -Width 8, 14 -Indent 2
+    Write-FormattedRow -Text "show", "<name>", ": Display manifest details" -Color Gray, DarkGray, Gray -Width 8, 14 -Indent 2
+    Write-FormattedRow -Text "source", "<name>", ": Set manifest repository source" -Color Gray, DarkGray, Gray -Width 8, 14 -Indent 2
+
+    Write-Host
+    Write-FormattedRow -Text "System Commands:" -Color Yellow
+    Write-FormattedRow -Text "install", "update", ": Update CLI scripts to the latest version" -Color Gray, Gray, Gray -Width 8, 14 -Indent 2
+    Write-Host
+    return
+}
+
+function Invoke-Required {
+    $paths = @(
+        $BaseDir
+        $ShimsDir
+        $ScriptsDir
+        $LocalDir
+        $TempDir
+    )
+    
+    New-Dirs -Paths $paths
+    $env:PATH += ";$ShimsDir"
+    $oldPath = [Environment]::GetEnvironmentVariable("PATH", "User")
+    if ($oldPath -notlike "*$ShimsDir*") {
+        [Environment]::SetEnvironmentVariable("PATH", "$oldPath;$ShimsDir", "User")
+    }
+    return
+}
+
+function Get-Headers {
+    param (
+        [switch]$Private,
+        [switch]$NoCache
+    )
+
+    $headers = @{}
+    $configFile = Join-Path $HOME ".zsetup"
+
+    if (Test-Path $configFile) {
+        try {
+            $config = Get-Content $configFile -Raw | ConvertFrom-Json
+            if ($config.apiKey) {
+                $headers["x-api-key"] = $config.apiKey
+            }
+
+            if ($Private) {
+                $headers["x-scope"] = 'private'
+            }
+            elseif ($config.scope) {
+                $headers["x-scope"] = $config.scope
+            }
+
+            if ($NoCache -or $config.noCache) {
+                $headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+                $headers["Pragma"] = "no-cache"
+            }
+        }
+        catch {
+            Write-FormattedRow -Text "Failed parsed", ": $configFile" -Color Yellow
+        }
+    }
+    return $headers
+}
+
+function Get-Aria2Headers {
+    param (
+        [switch]$Private,
+        [switch]$NoCache
+    )
+
+    $aria2Headers = @{}
+    $headers = Get-Headers -Private:$Private -NoCache:$NoCache
+    foreach ($header in $headers.GetEnumerator()) {
+        $aria2Headers += "--header=$($header.Key): $($header.Value)"
+    }
+    return $aria2Headers
+}
+
+function Get-Manifest {
+    param (
+        [string]$Name,
+        [string]$Uri,
+        [switch]$Private,
+        [switch]$NoCache
+    )
+
+    try {
+        $uri = $Uri ? $Uri : "$BaseUrl/getManifest/$Name"
+        $headers = Get-Headers -Private:$Private -NoCache:$NoCache
+        Invoke-RestMethod -Uri $uri -Headers $headers
+    }
+    catch {
+        return $null
+    }
+}
+
+function Invoke-Download {
+    param (
+        [string]$Method,
+        [string]$Uri,
+        [string]$OutDir,
+        [string]$OutName,
+        [switch]$Private,
+        [switch]$NoCache
+    )
+
+    function Invoke-Download {
+        $outPath = Join-Path $OutDir $OutName
+        $headers = Get-Headers -Private:$Private -NoCache:$NoCache
+        Invoke-WebRequest -Uri $Uri -Headers $headers -OutFile $outPath
+    }
+
+    function Invoke-Aria2Download {
+        $headers = Get-Aria2Headers -Private:$Private -NoCache:$NoCache
+        aria2c -s 16 -x 16 -K 1M --allow-overwrite=true `
+            -d $OutDir -o $OutName $headers $Uri | Out-Null
+    }
+
+    switch ($Method) {
+        'invoke' {
+            Invoke-Download
+        }
+
+        'aria2' {
+            try {
+                Invoke-Aria2Download
+            }
+            catch {
+                Invoke-Download
+            }
+        }
+    }
+    return
+}
+
+function Invoke-Extract {
+    param (
+        [string]$Method,
+        [string]$Archive,
+        [string]$OutDir,
+        [psobject]$Password
+    )
+
+    function Expand-WithTar {
+        tar -xf $Archive -C $OutDir | Out-Null
+    }
+
+    function Expand-With7z {
+        if ($Password) {
+            7z x $Archive -o"$OutDir" -p"$Password" -y | Out-Null
+        }
+        else {
+            7z x $Archive -o"$OutDir" -y | Out-Null
+        }
+    }
+
+    New-Dirs -Paths $OutDir
+    switch ($Method) {
+        'tar' {
+            Expand-WithTar
+        }
+
+        '7z' {
+            try {
+                Expand-With7z
+            }
+            catch {
+                Expand-WithTar
+            }
+        }
+
+    }
+    return
+}
+
+function New-Dirs {
+    param (
+        [array]$Paths = @()
+    )
+
+    foreach ($path in $Paths) {
+        if (-not(Test-Path $path -ErrorAction SilentlyContinue)) {
+            New-Item -ItemType Directory -Path $path -Force | Out-Null
+        }
+    }
+    return
+}
+
+function Remove-Items {
+    param (
+        [array]$Paths = @()
+    )
+
+    foreach ($path in $Paths) {
+        if (Test-Path $path -ErrorAction SilentlyContinue) {
+            Remove-Item -Path $path -Recurse -Force | Out-Null
+        }
+    }
+    return
+}
+
+function Copy-Items {
+    param (
+        [hashtable]$Paths = @{}
+    )
+    
+    foreach ($path in $Paths.GetEnumerator()) {
+        New-Dirs -Paths $path.Value
+        Copy-Item -Path $path.Key -Destination $path.Value -Recurse -Force | Out-Null        
+    }
+    return
+}
+
+function Expand-Path {
+    param (
+        [string]$Path
+    )
+
+    $pathsMap = @{
+        '<BaseDir>'    = $BaseDir
+        '<ShimsDir>'   = $ShimsDir
+        '<ScriptsDir>' = $ScriptsDir
+        '<LocalDir>'   = $LocalDir
+        '<TempDir>'    = $TempDir
+    }
+
+    foreach ($pathMap in $pathsMap.GetEnumerator()) {
+        $Path = $Path.Replace($pathMap.key, $pathMap.Value)
+    }
+    return $Path
+}
+
+function Invoke-Shim {
+    param (
+        [string]$Path,
+        [string]$Name
+    )
+
+    if (-not (Test-Path $Path)) {
+        Write-FormattedRow -Text "Not found!", ": $Path" -Color Yellow
+        return
+    }
+    
+    $ext = [System.IO.Path]::GetExtension($Path).ToLower()
+    $shimPath = Join-Path $ShimsDir "$Name.cmd"
+    $exec = switch ($ext) {
+        '.ps1' { "powershell -ExecutionPolicy Bypass -NoProfile -File ""$Path"" %*" }
+        '.py' { "python ""$Path"" %*" }
+        default { """$Path"" %*" }
+    }
+
+    Set-Content -Path $shimPath -Value "@echo off`n$exec" -Encoding ASCII -Force
+    return
+}
+
+function Invoke-Shortcut {
+    param (
+        [string]$Path,
+        [string]$Name
+    )
+
+    $wshShell = New-Object -ComObject WScript.Shell
+    $desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
+    $shortcutPath = Join-Path $desktopPath "$Name.lnk"
+    $shortcut = $wshShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $Path
+    $shortcut.WorkingDirectory = [System.IO.Path]::GetDirectoryName($Path)
+    $shortcut.Save()
+    return
+}
+
+function Clear-Shims {
+    Get-ChildItem -Path (Join-Path $ShimsDir "*.cmd") -ErrorAction SilentlyContinue | ForEach-Object {
+        if ((Get-Content $_.FullName -Raw) -match '"([^"]+\.exe)"' -and -not (Test-Path $Matches[1])) {
+            Remove-Items -Paths $_.FullName
+        }
+    }
+    return
+}
+
+function Write-FormattedRow {
+    param(
+        [string[]]$Text,
+        [ConsoleColor[]]$Color,
+        [int[]]$Width,
+        [int]$Indent
+    )
+
+    if (-not $Color -or $Color.Count -eq 0) { 
+        $Color = @('Gray') 
+    }
+
+    if (-not $Width -or $Width.Count -eq 0) {
+        $Width = @(20) 
+    }
+
+    if (-not $Indent) {
+        $Indent = 0 
+    }
+
+    if ($Indent -gt 0) {
+        Write-Host (" " * $Indent) -NoNewline
+    }
+
+    for ($i = 0; $i -lt $Text.Count; $i++) {
+        $cellText = $Text[$i]
+        $cellColor = if ($i -lt $Color.Count) { $Color[$i] } else { $Color[-1] }
+
+        if ($i -eq ($Text.Count - 1)) {
+            Write-Host $cellText -ForegroundColor $cellColor
+        }
+        else {
+            $cellWidth = if ($i -lt $Width.Count) { $Width[$i] } else { $Width[-1] }
+            $paddedText = "{0,-$cellWidth}" -f $cellText
+            Write-Host $paddedText -NoNewline -ForegroundColor $cellColor
+        }
+    }
+    return
+}
+
+function Write-Manifest {
+    param (
+        [psobject]$Manifest
+    )
+    
+    $manifestMap = [ordered]@{
+        "ID"               = $Manifest.Id
+        "Name"             = $Manifest.Name
+        "Version"          = $Manifest.Version
+        "Publisher"        = $Manifest.Publisher
+        "Description"      = $Manifest.Description
+        "Aliases"          = $Manifest.Aliases
+        "URL"              = $Manifest.Url
+        "Archive Type"     = $Manifest.ArchiveType
+        "Output Name"      = $Manifest.OutName
+        "Output Dir"       = $Manifest.OutDir
+        "Output Path"      = $Manifest.OutPath
+        "Download Method"  = $Manifest.DownloadMethod
+        "Extract Method"   = $Manifest.ExtractMethod
+        "Extract Password" = $Manifest.ExtractPassword
+        "Install Type"     = $Manifest.InstallType
+        "Extract Dir"      = $Manifest.ExtractDir
+        "Install Dir"      = $Manifest.InstallDir
+        "Executable Path"  = $Manifest.ExecutablePath
+        "Shortcut Type"    = $Manifest.ShortcutType
+        "Shortcut Name"    = $Manifest.ShortcutName
+    }
+
+    if ($Manifest) {
+        Write-Host
+        foreach ($manifest in $manifestMap.GetEnumerator()) {
+            if ($manifest.Value) {
+                Write-FormattedRow -Text $manifest.Key, ": $(Expand-Path -Path $manifest.Value)"
+            }
+        }
+        Write-Host
+    }
+    return
+}
+
+function Invoke-Search {
+    param (
+        [string]$Name,
+        [switch]$Private,
+        [switch]$NoCache
+    )
+    
+    $manifestIndex = Get-Manifest -Uri "$BaseUrl/getIndexManifest" -Private:$Private -NoCache:$NoCache
+    if (-not $manifestIndex) {
+        Write-FormattedRow -Text "Not found!", ": manifests-index.json" -Color Yellow
+        return
+    }
+
+    $results = $manifestIndex | Where-Object {
+        $_.id -like "*$Name*" -or
+        $_.name -like "*$Name*" -or
+        ($_.aliases | Where-Object { $_ -like "*$Name*" })
+    }
+
+    if (-not $results -or $results.Count -eq 0) {
+        Write-FormattedRow -Text "Not found!", ": $Name" -Color Yellow
+        return
+    }
+
+    $widths = @(35, 24, 10, 40)
+    $totalWidth = ($widths | Measure-Object -Sum).Sum
+
+    Write-FormattedRow -Text "ID", "Name", "Version", "Description" -Width $widths
+    Write-Host ("-" * $totalWidth ) -ForegroundColor DarkGray
+
+    foreach ($result in $results) {
+        if ($result.description) {
+            $findDescription = $result.description
+            if ($findDescription.Length -gt 40) {
+                $description = $findDescription.Substring(0, 37) + "..."
+            }
+            else {
+                $description = $findDescription 
+            }
+        }
+        else { 
+            $description = "-" 
+        }
+
+        Write-FormattedRow -Text $result.id, $result.name, $result.version, $description -Width $widths
+    }
+    return
+}
+
+function Show-Manifest {
+    param (
+        [string]$Name,
+        [switch]$Private,
+        [switch]$NoCache
+    )
+
+    $manifest = Get-Manifest -Name $Name -Private:$Private -NoCache:$NoCache
+    if (-not $manifest) { 
+        Invoke-Search -Name $Name -Private:$Private -NoCache:$NoCache
+        return
+    }
+    
+    Write-Manifest -Manifest $manifest
+    return
+}
+
+function Set-State {
+    param (
+        [switch]$Private,
+        [switch]$Public,
+        [switch]$NoCache,
+        [switch]$Cache
+    )
+
+    $config = @{}
+    $configFile = Join-Path $HOME ".zsetup"
+
+    if (Test-Path $configFile) {
+        $jsonContents = Get-Content $configFile -Raw | ConvertFrom-Json
+        if ($jsonContents) {
+            foreach ($content in $jsonContents.psobject.properties) {
+                $config[$content.Name] = $content.Value
+            }
+        }
+    }
+
+    if ($Private) {
+        $config["scope"] = 'private' 
+    }
+    elseif ($Public) {
+        $config["scope"] = 'public' 
+    }
+
+    if ($NoCache) {
+        $config["noCache"] = $true 
+    }
+    elseif ($Cache) {
+        $config["noCache"] = $false 
+    }
+
+    $config | ConvertTo-Json | Set-Content $configFile
 }
 
 Clear-Shims
 Test-Command -Action $Action -Name $Name -Help:$Help
 
 switch ($Action) {
-    'search' {
-        Get-Search -BaseUrl $BaseUrl -Name $Name -Source $Source
-    }
-
-    'source' {
-        Set-Source -Name $Name
-    }
-
-    'show' {
-        Get-Show -BaseUrl $BaseUrl -Name $Name -Source $Source
-    }
-
-    'install' {
-        Get-Required -BaseUrl $BaseUrl
-        Get-Install -BaseUrl $BaseUrl -Name $Name -Source $Source -Headers (Get-BootstrapHeaders)
+    default {
+        Show-Help
     }
 }
